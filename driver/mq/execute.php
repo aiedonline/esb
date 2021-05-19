5831c196afeff229537fa743cab51d987d590dd8
Wellington <wellington.oliveira@cps.sp.gov.br>
2021-05-18 22:08:41 -0300

    Versão 1.0

--- driver/mq/execute.php
+++ driver/mq/execute.php
@@ -1,9 +1,8 @@
 <?php
-require_once dirname(dirname(dirname(__FILE__))) . "/api/utilitario.php";
-//require_once dirname(dirname(dirname(__FILE__))) . "/api/json_v2.php";
-require_once dirname(dirname(dirname(__FILE__))) . "/api/edb.php";
+error_reporting(0);
 
-// teste
+require_once dirname(dirname(dirname(__FILE__))) . "/api/utilitario.php";
+require_once dirname(dirname(dirname(__FILE__))) . "/api/edb.php";
 
 
 class Drivermq {
@@ -12,80 +11,79 @@ class Drivermq {
 		$now = DateTime::createFromFormat('U.u', microtime(true));
 		switch ($post_data['action'] ){
 			case 'register':
+				//
 				$path_work = dirname(dirname(dirname(__FILE__))) . "/data/mq/" . $post_data['work'] . ".json";
 				$work = Json::FromFile($path_work);
 				if($work != null) {
-					$fid = $now->format("Y-m-d H:i:s.u"); //microtime(true) * 1000000;
+					$fid = strval($now->format("Y-m-d H:i:s.u"));
+					if($post_data['fid'] != ""){
+						$fid = $post_data['fid'];
+					}
 					$t  = $now->format("Y-m-d H:i:s.u");
 					$buffer = array("fid" => $fid, "noid" => $post_data['noid'], "task" => $work->tasks[0], "workgroup" => $post_data['group'], "work" => $post_data['work'],
-					"script" =>  $work->scripts[0], "request" => $post_data['request'], "time" => $t, "wait_time" => $t);
-					if( JsonV2::WriteFile(dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/" . $work->tasks[0], strval($fid), $buffer) == true) {
-						return  array( "fid" => $fid, 'time' => $t );
+					"script" =>  $work->scripts[0],  "request" => [] , "time" => $t, "wait_time" => $t);
+					array_push($buffer['request'], $post_data['request'] );
+					$path_to_dir = dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/" . $work->tasks[0];
+					$path_to_all = dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/no_task_all";
+					
+					if (!file_exists($path_to_all)) {
+						mkdir($path_to_all, 0777, true);
+					}
+					if (!file_exists($path_to_all . "/" . $fid)) {
+						if( JsonV2::WriteFile($path_to_dir, $fid, $buffer) == true) {
+							link($path_to_dir . "/" . $fid, $path_to_all . "/" . $fid);
+							return  array( "fid" => $fid, 'time' => $t );
+						} else {
+							throw new Exception('Falha ao salvar a atividade na MQ');
+						}
 					} else {
-						throw new Exception('Falha ao salvar a atividade na MQ');
+						return  array( "fid" => $fid, 'time' => $t );
 					}
 				} else {
 					throw new Exception('Não existe trabalho!');
 				}
 				break;
-			case 'do':
-				//$dados = Database::Data('task', ['fid'], [$post_data['fid']]);
-				// Pega o primeiro elemento da atividade
-				//if(count($dados) > 0){
-				//	$dados = $dados[0];
-				//	// retorno do cliente
-				//	echo Utilitario::SaidaPadrao(true, $dados['data'], "entry");
-				//	$now = $now->modify('+5 minutes');
-				//	Database::Write('task', ['fid', 'wait_time'], [$dados['data']['fid'], $now->format("Y-m-d H:i:s.u")], '');
-				//}
-			break;
 			case 'which':
-				$retorno = [];
+				$retorno = array();
 				
 				$works = scandir($ROOT . "/data/mq/");
-				
 				for($i = 0; $i < count($works); $i++){
-					error_log( $ROOT . "/data/mq/" . $works[$i], 0);
 					$work = Json::FromFile($ROOT . "/data/mq/" . $works[$i]);
 					if($work == null) continue;
 
 					$buffer = array("work" => $work->name, "tasks" => [], "group" => "");
 					for($j = 0; $j < count($work->tasks) - 1; $j++){
 						for($k = 0; $k < count($post_data['groups']); $k++){
-							error_log($ROOT . "/tmp/mq/" . $post_data['groups'][$k] . "/" . $work->tasks[$j], 0);
-							if( file_exists($ROOT . "/tmp/mq/" . $post_data['groups'][$k] . "/" . $work->tasks[$j] ) == true){
-								if(count(scandir($ROOT . "/tmp/mq/" . $post_data['groups'][$k] . "/" . $work->tasks[$j] )) > 0 ) {
+							$diretorio_tarefa = $ROOT . "/tmp/mq/" . $post_data['groups'][$k] . "/" . $work->name  . "/" . $work->tasks[$j];
+							if( file_exists(  $diretorio_tarefa ) ){
+								if(count(scandir($diretorio_tarefa)) > 2 ) {
 									$buffer["group"] = $post_data['groups'][$k];
-									$buffer["tasks"].append($work->tasks[$j]);
+									array_push($buffer["tasks"],$work->tasks[$j] );
 								}
-								
 							}
 						}
 					}
-					if(count($buffer["tasks"]) > 0){
-						$retorno.append($buffer);
+					if(count($buffer["tasks"]) > 0) {
+						array_push($retorno, $buffer);
 					}
 				}
 				
 				return $retorno;
 				break;
 			case 'next':
-				
-
-
-				/*if ($handle = opendir(dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['task'])) {
+				$path_to_task = dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/" . $post_data['task'] . "/";
+				if ($handle = opendir($path_to_task)) {
 					try{
 						while (false !== ($entry = readdir($handle))) {
-							$path_to_file = dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['task'] . "/".  $entry;
+							$path_to_file = $path_to_task .  $entry;
 							$buffer = JsonV2::FromFile( $path_to_file );
 							if($buffer != null){
-								if( date("Y-m-d H:i:s.u", $buffer->wait_time) < $now ){
-									// achou.... já trava uma data
-									$now->modify('+5 minutes');
-									$buffer->wait_time = $now->format("Y-m-d H:i:s.u");
-									JsonV2::WriteFile(dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['task'], $entry, $buffer);
-									return $buffer;
-								}
+								$path_work = dirname(dirname(dirname(__FILE__))) . "/data/mq/" . $post_data['work'] . ".json";
+								$work = Json::FromFile($path_work);
+								unlink($path_to_task . $entry);
+								$task_index = array_search($post_data['task'], $work->tasks);
+								$buffer->script = $work->scripts[$task_index];
+								return $buffer;
 							}
 						}
 					} catch (Exception $e) {
@@ -94,9 +92,8 @@ class Drivermq {
 					}
 					finally {
 						closedir($handle);
-						error_log("Diretório fechado");
 					}
-				} */
+				} 
 			break;
 			case 'status':
 				// .. com FID pesquisa onde está .../
@@ -115,19 +112,53 @@ class Drivermq {
 				//}
 			break;
 			case 'response':
-				$path_to_file = dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['task_from'] . "/".  $post_data['fid'];
-				$buffer = JsonV2::FromFile( $path_to_file );
-				error_log( $path_to_file , 0);
-				if($buffer == null){
-					throw new Exception('Não existe o arquivo:  ' . $post_data['fid']);
-				}
-				$buffer->request = $post_data['request'];
-				$buffer->response = $post_data['response'];
-				if ( JsonV2::WriteFile(dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['task_to'], $post_data['fid'], $buffer) == true) {
-					unlink($path_to_file);
-					return true;
+				$path_to_work = dirname(dirname(dirname(__FILE__))) . "/data/mq/" . $post_data['work'] . ".json";
+				$work = JsonV2::FromFile( $path_to_work );
+				if($work != null) {
+					$task_index = array_search($post_data['task'], $work->tasks) + 1;
+					
+					$path_to_file 			= dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/no_task_all/".  $post_data['fid'];
+					$path_to_new_directory 	= dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/" . $work->tasks[$task_index] . "/";
+					$buffer = JsonV2::FromFile( $path_to_file );
+					if($buffer == null){
+						throw new Exception('Não existe o arquivo:  ' . $post_data['fid']);
+					}
+					array_push($buffer->request, $post_data['response']);
+					
+					if(!file_exists($path_to_new_directory)){
+						mkdir($path_to_new_directory, 0777, true);
+					}
+					if ( link(  $path_to_file, $path_to_new_directory . $post_data['fid'] ) == 0) {
+						return true;
+					} else {
+						throw new Exception('Não foi possível salver o trabalho no próximo task');
+					}
 				} else {
-					throw new Exception('Não foi possível salver o trabalho no próximo task');
+					// work nao encontrado...
+				}
+			break;
+			case 'error':
+				$path_to_work = dirname(dirname(dirname(__FILE__))) . "/data/mq/" . $post_data['work'] . ".json";
+				$work = JsonV2::FromFile( $path_to_work );
+				if($work != null) {
+					$path_to_file 			= dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/" . $post_data['task'] . "/".  $post_data['fid'];
+					$path_to_new_directory 	= dirname(dirname(dirname(__FILE__))) . "/tmp/mq/". $post_data['group'] . "/" . $post_data['work'] . "/no_task_fail/" . $post_data['task'] . "/";
+					$buffer = JsonV2::FromFile( $path_to_file );
+					if($buffer == null){
+						throw new Exception('Não existe o arquivo:  ' . $post_data['fid']);
+					}
+					$buffer->output = $post_data['output'];
+					$buffer->code = $post_data['code'];
+					if(!file_exists($path_to_new_directory)){
+						mkdir($path_to_new_directory, 0777, true);
+					}
+					if ( link($path_to_file, $path_to_new_directory .  $post_data['fid'] ) == 0) {
+						return true;
+					} else {
+						throw new Exception('Não foi possível salver o trabalho no próximo task');
+					}
+				} else {
+					// work nao encontrado...
 				}
 			break;
 		}
--- execute.php
+++ execute.php
@@ -1,10 +1,11 @@
 <?php
+error_reporting(0);
 require_once __DIR__ . "/api/utilitario.php";
 require_once __DIR__ . "/api/json_v2.php";
 require_once __DIR__ . "/api/edb.php";
 
-//error_reporting(0);
-error_reporting(E_ALL);
+
+//error_reporting(E_ALL);
 
 
 // teste
@@ -15,8 +16,8 @@ try {
     $part = explode("/", $_SERVER["REQUEST_URI"]);
     $post_data = json_decode(file_get_contents('php://input'), true);
 
-    error_log("\n\n\n\n", 0);
-    error_log(json_encode($post_data), 0);
+    #error_log("\n\n\n\n", 0);
+    #error_log(json_encode($post_data), 0);
 
     require_once __DIR__ . "/driver/". $post_data["driver"] ."/execute.php";
     //$ex = new DriverEDB();
